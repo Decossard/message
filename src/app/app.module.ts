c976b59146a5a70c540c44f82e46288797647628
hyaadd <hyaad.decossard@gmail.com>
2018-06-21 00:16:14 -0400

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TermsNPrivacyComponent } from './terms-n-privacy/terms-n-privacy.component';
+import { FeedComponent } from './feed/feed.component';
+import { SearchComponent } from './search/search.component';
+import { TextEditorComponent } from './text-editor/text-editor.component';
+import { PhoneNumberComponent } from './phone-number/phone-number.component';
+import { ActivationCodeComponent } from './activation-code/activation-code.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TermsNPrivacyComponent,
+    FeedComponent,
+    SearchComponent,
+    TextEditorComponent,
+    PhoneNumberComponent,
+    ActivationCodeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/terms-n-privacy/terms-n-privacy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-n-privacy',
+  templateUrl: './terms-n-privacy.component.html',
+  styleUrls: ['./terms-n-privacy.component.css']
+})
+export class TermsNPrivacyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
