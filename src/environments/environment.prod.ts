b5a01061fe016a6096e89c2453733406bdf2e075
hyaadd <hyaad.decossard@gmail.com>
2018-06-21 12:59:56 -0400

    minor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,5 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyD3-H7s5J80Fu6vADmN-tJJWc9XWGzFajo",
-    authDomain: "message-9c5a8.firebaseapp.com",
-    databaseURL: "https://message-9c5a8.firebaseio.com",
-    projectId: "message-9c5a8",
-    storageBucket: "",
-    messagingSenderId: "232958469019"
   }
 };
