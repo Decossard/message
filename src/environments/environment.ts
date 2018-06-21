50119c995cb7e55efb18fad7ac64abc2afd8093a
hyaadd <hyaad.decossard@gmail.com>
2018-06-21 00:30:33 -0400

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD3-H7s5J80Fu6vADmN-tJJWc9XWGzFajo",
+    authDomain: "message-9c5a8.firebaseapp.com",
+    databaseURL: "https://message-9c5a8.firebaseio.com",
+    projectId: "message-9c5a8",
+    storageBucket: "",
+    messagingSenderId: "232958469019"
+  }
 };
 
 /*
